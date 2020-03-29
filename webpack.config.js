5d7ffb1eac053948e76b909c387988a64b5c1870
a.360 <a.360@icloud.com>
2020-03-29 19:47:19 +0900

    config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.tsx",
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+    	filename: "bundle.js",
+
+    },
+	module: {
+    	rules: [
+            {
+		        test: /\.tsx?$/,
+				exclude: /node_modules/,
+                use: "ts-loader"
+            },
+			{
+				test: /\.scss/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader"
+				]
+			},
+    	]
+    },
+	resolve: {
+    	extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+	devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		open: true,
+    },
+	externals: {
+    	"react": "React",
+    	"react-dom": "ReactDOM"
+    },
+	plugins: [
+		new HtmlWebpackPlugin({template: './src/index.html'})
+	]
+};
+	
